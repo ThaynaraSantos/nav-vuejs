b9f4d5b67ce7bde657c650d1a5c2dd68f084154a
Thaynara Santos <thaynaracsantos@gmail.com>
2018-02-18 17:52:21 -0300

    Set active item with router link

--- nav/nav.js
+++ nav/nav.js
@@ -1,13 +1,16 @@
+const router = new VueRouter()
+
 var nav = new Vue({
+  router,
   el: '#nav',
   data: {
-	home: {name: "Navbar", link: "#home"},
+	home: {name: "Navbar", link: "home"},
 	items: [
-		{name: "About", link: "#about"}, 
-		{name: "Services", link: "#services"}, 
-		{name: "Products", link: "#products"},
-		{name: "Pricing", link: "#pricing"},
-		{name: "Contact", link: "#contact"}
+		{name: "About", link: "about"}, 
+		{name: "Services", link: "services"}, 
+		{name: "Products", link: "products"},
+		{name: "Pricing", link: "pricing"},
+		{name: "Contact", link: "contact"}
 	]
   }
 })
